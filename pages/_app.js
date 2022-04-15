74e6a5fb49a1af31f05e0390a6521d513e7a5868
Charles Craven-Bartle <ccravenbartle@gmail.com>
2022-04-15 16:01:23 +0100

    220415 1601

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
+import React from "react";
 
-import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-datepicker/dist/react-datepicker.css";
+import "react-toastify/dist/ReactToastify.css";
+import "slate-simple-editor/dist/index.css";
+import "@/styles/main.scss";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-datepicker/dist/react-datepicker.css';
-import 'react-toastify/dist/ReactToastify.css';
-import 'slate-simple-editor/dist/index.css';
-import '@/styles/main.scss';
-
-const App = ({Component, pageProps}) =>
-  <Component {...pageProps} />
+const App = ({ Component, pageProps }) => {
+  console.log(process.env.AUTH0_DOMAIN);
+  console.log(process.env.AUTH0_REDIRECT_URI);
+  return <Component {...pageProps} />;
+};
 
 export default App;
--- server.js
+++ server.js
@@ -1,21 +1,24 @@
+const express = require("express");
+const next = require("next");
 
-const express = require('express');
-const next = require('next');
-
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({dev});
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
 
-  server.all('*', (req, res) => {
+  server.all("*", (req, res) => {
     return handle(req, res);
-  })
+  });
 
+  console.log("*** Environment: " + process.env.NODE_ENV);
+  console.log("*** Port: " + process.env.PORT);
   const PORT = process.env.PORT || 3000;
   server.listen(PORT, (err) => {
-    if (err) { console.log(err); }
-    console.log(`> Ready on port ${PORT}`)
-  })
-})
+    if (err) {
+      console.log(err);
+    }
+    console.log(`> Ready on port ${PORT}`);
+  });
+});
